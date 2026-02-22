b4957683886a312ea60701a7262ec2f61a00d735
omrane-linux <omranee07@gmail.com>
2026-02-22 17:58:17 +0530

    Hardcode Render URL for Netlify deployment

--- frontend/expense tracker/src/utils/apipaths.js
+++ frontend/expense tracker/src/utils/apipaths.js
@@ -1,4 +1,4 @@
-export const BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:8000";
+export const BASE_URL = "https://financial-ulj9.onrender.com";
 
 export const API_PATHS = {
   AUTH: {
